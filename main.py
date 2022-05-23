0334a31648fdd9731ad161cd00f46d7682e09a21
Your Name <you@example.com>
2022-05-23 13:22:49 +0300

    Готово.

--- main.py
+++ main.py
@@ -49,23 +49,6 @@ def start(update: Update, context: CallbackContext) -> int:
     return CHOOSING # Возвращает одно из состояний диалога.
 
 
-# def enter_name(update: Update, context: CallbackContext) -> int:
-#     """Ask the user for info about the selected predefined choice."""
-#     text = update.message.text
-#     context.user_data['choice'] = text
-#     # update.message.reply_text(f'Your {text.lower()}? Yes, I would love to hear about that!')
-#     update.message.reply_text('Введите Ваше имя:')
-#     return TYPING_REPLY
-
-# def regular_choice(update: Update, context: CallbackContext) -> int:
-#     """Ask the user for info about the selected predefined choice."""
-#     text = update.message.text
-#     context.user_data['choice'] = text
-#     # update.message.reply_text(f'Your {text.lower()}? Yes, I would love to hear about that!')
-#     update.message.reply_text('Имя?')
-#     return TYPING_REPLY
-
-
 def custom_choice(update: Update, context: CallbackContext) -> int:
     """Функция записывает информация пользователя в зависимости от нажатой кнопки."""
     text = update.message.text
@@ -83,14 +66,16 @@ def done(update: Update, context: CallbackContext) -> int:
     if 'choice' in user_data:
         del user_data['choice']
 
+    message = f"Ваши итоговые данные:\n{facts_to_str(user_data)}"
+    if not user_data:
+        message = "Вы ничего не ввели.\n"
+
     update.message.reply_text(
-        "Ваши итоговые данные:"
-        f"{facts_to_str(user_data)}"
-        "Если захотите поменять данные, введите команду /start",
+        message + "\nЕсли захотите поменять данные, введите команду /start",
         reply_markup=ReplyKeyboardRemove(), # Удаляем клавиатуру.
     )
 
-    main_page(update, context)
+    courses(update, context)
 
     return ConversationHandler.END
 
@@ -104,54 +89,59 @@ def received_information(update: Update, context: CallbackContext) -> int:
     del user_data['choice'] # Бот забывает ненужную часть информации о пользователе (Фамилия|Имя|Группа|Дисциплина).
 
     update.message.reply_text(
-        "Ваши данные:"
+        "Ваши данные:\n"
         f"{facts_to_str(user_data)}"
-        'Если Вы всё заполнили, нажмите кнопку "Готово"',
+        '\nЕсли Вы всё заполнили, нажмите кнопку "Готово"',
         reply_markup=markup,
     )
 
     return CHOOSING
 
 
-def main_page(update: Update, context: CallbackContext):
-    keyboard = [
-        [InlineKeyboardButton("Верхний блок навигации", callback_data='navigation')],
-        [InlineKeyboardButton("На страницу обращений", callback_data='1')],
-        [InlineKeyboardButton("На страницу общих материалов", callback_data='2')],
-        [InlineKeyboardButton("На страницу курсов", callback_data='courses')],
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    user = update.effective_user # Telegram-nickname пользователя.
-    user_data = context.user_data # То, что пользователь ввёл в поле "Имя".
-    # Умная система здоровается с пользователем по тому, что он ввёл в имя или по его Telegram-никнейму, если не введено первое.
-    update.message.reply_markdown_v2(
-        fr'''Привет, {user_data.get('Имя', user.mention_markdown_v2())}\!
-Главная страница
-        ''',
-        reply_markup=reply_markup,
-    )
+# def main_markup(update: Update, context: CallbackContext):
+#     keyboard = [
+#         # [InlineKeyboardButton("Верхний блок навигации", callback_data='navigation')],
+#         [InlineKeyboardButton("Обращения", callback_data='1')],
+#         # [InlineKeyboardButton("Общие материалы", callback_data='2')],
+#         [InlineKeyboardButton("Курсы", callback_data='courses')],
+#     ]
+#     reply_markup = InlineKeyboardMarkup(keyboard)
+#     user = update.effective_user # Telegram-nickname пользователя.
+#     user_data = context.user_data # То, что пользователь ввёл в поле "Имя".
+#     return {
+#         "text": fr'''Привет, {user_data.get('Имя', user.mention_markdown_v2())}\!
+# Главная страница
+#         ''',
+#         "reply_markup":reply_markup,
+#     }
 
 
-def navigation_markup():
-    keyboard = [
-        [InlineKeyboardButton("Главная страница", callback_data='0')],
-        [InlineKeyboardButton("Таблица баллов", callback_data='1')],
-        [InlineKeyboardButton("Настройка курсов", callback_data='2')],
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    return reply_markup
+# def main_page(update: Update, context: CallbackContext):
+#     # Умная система здоровается с пользователем по тому, что он ввёл в имя или по его Telegram-никнейму, если не введено первое.
+#     update.message.reply_markdown_v2(**main_markup(update, context))
 
 
-def navigation(update: Update, context: CallbackContext):
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'''Hi {user.mention_markdown_v2()}\!
-Страница навигации
-        ''',
-        reply_markup=navigation_markup(),
-    )
+# def navigation_markup():
+#     keyboard = [
+#         [InlineKeyboardButton("Главная страница", callback_data='main')],
+#         [InlineKeyboardButton("Настройка курсов", callback_data='1')],
+#     ]
+#     reply_markup = InlineKeyboardMarkup(keyboard)
+#     return reply_markup
 
 
+# def navigation(update: Update, context: CallbackContext):
+#     user = update.effective_user
+#     update.message.reply_markdown_v2(
+#         fr'''Hi {user.mention_markdown_v2()}\!
+# Страница навигации
+#         ''',
+#         reply_markup=navigation_markup(),
+#     )
+
+
+# Без markup - назначаем на команду Telegram со /
+# С markup - формирование сообщения.
 def courses_markup(context: CallbackContext):
     user_data = context.user_data
 
@@ -168,79 +158,123 @@ def courses_markup(context: CallbackContext):
         # Только в случаях, если общий для всех курс, если пользователь не ввёл группу, или если в списке курса есть группа пользователя, то выводим соответствующие курсы.
         if common or user_data.get("Группа", "") == "" or user_data.get("Группа", "") in groups:
             keyboard.append([InlineKeyboardButton(c['name'], callback_data='course-'+str(c['id']))])
+    # keyboard.append([InlineKeyboardButton("К главной странице", callback_data='main')])
     reply_markup = InlineKeyboardMarkup(keyboard)
     return reply_markup
 
 
 def courses(update: Update, context: CallbackContext):
-    user_data = context.user_data
-    # print("Data: ", user_data)
+    user_data = context.user_data # Для того, что пользователь ввёл в поле "Имя".
+    user = update.effective_user # Telegram-nickname пользователя.
+    # Умная система здоровается с пользователем по тому, что он ввёл в имя или по его Telegram-никнейму, если не введено первое.
+    hello = fr"Привет, {user_data.get('Имя', user.mention_markdown_v2())}\!"
     if "Группа" in user_data:
-        msg = f'Курсы для группы {user_data["Группа"]}:'
+        msg = hello + f'\nКурсы для группы {user_data["Группа"]}:'
     else:
-        msg = 'Все курсы:' # Если пользователь не ввёл группу, то показываются все курсы.
+        msg = hello + '\nВсе курсы:' # Если пользователь не ввёл группу, то показываются все курсы.
     update.message.reply_markdown_v2(
         msg,
         reply_markup=courses_markup(context),
     )
 
 
+def course_description(course, teacher):
+
+    message = f'''{course["name"]}
+
+Преподаватель: {teacher["name"]} (@{teacher["telegram_nickname"]})\nТаблица баллов преподавателя:\n{teacher["table_link"]}
+
+Группы: '''
+
+    groups = []
+    for g in course['course_groups']:
+        groups.append(g['name'])
+
+    message += ', '.join(groups)
+
+    message += f'\n\nПравила:\n{course["rules"]}\n\n'
+
+    message += "Материалы:\n\n"
+
+    for m in course['course_materials']:
+        message += f'{m["material_name"]}, ссылка: {m["link"]}\n'
+
+    # message += "\nЛабораторные:\n\n"
+
+    # labs = []
+    # for lab in course['course_labs']:
+    #     lab_info = requests.get(f'{API_URL}/lab/{lab["id"]}').json()
+    #     message += f'{lab["number"]} с дедлайном {lab["deadline"]}.\nСсылки:\n'
+    #     for link in lab_info.get('links', []):
+    #         message += link['link'] + "\n"
+
+    return message
+
+
 def course_markup(id):
-    r = requests.get(f'{API_URL}/course/{id}').json()
     keyboard = []
-    for c in r:
-        keyboard.append([InlineKeyboardButton(c['name'], callback_data='0')])
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    return reply_markup
+    r = requests.get(f'{API_URL}/course/{id}').json()
+    t = requests.get(r['teacher']).json()
+
+    message = course_description(r, t)
+
+    for lab in r['course_labs']:
+        keyboard.append([InlineKeyboardButton("Лабораторная " + lab['number'], callback_data='lab-'+str(lab['id']))])
+
+    keyboard.append([InlineKeyboardButton("К списку курсов", callback_data='courses')])
+
+    return {"text": message, "reply_markup": InlineKeyboardMarkup(keyboard)}
 
 
-def course(update: Update, context: CallbackContext):
-    user_data = context.user_data
-    update.message.reply_markdown_v2(
-        'Курс ...',
-        reply_markup=course_markup(),
-    )
+def lab_markup(id):
+    keyboard = []
+    lab = requests.get(f'{API_URL}/lab/{id}').json()
+
+    course_id = lab["course"].strip("/").split("/")[-1]
+
+    course = requests.get(f'{API_URL}/course/{course_id}').json()
+    teacher = requests.get(course['teacher']).json()
+
+    message = course_description(course, teacher) + "\n"
+
+    message += f"Лабораторная {lab['number']}:\n\n"
+
+    message += f"Дедлайн: {lab['deadline']}\n\n"
+
+    message += "Ссылки:\n"
+
+    for link in lab.get('links', []):
+        message += link['link'] + "\n"
+
+    keyboard.append([InlineKeyboardButton("Написать преподавателю", url=f"tg://resolve?domain={teacher['telegram_nickname']}")])
+    # keyboard.append([InlineKeyboardButton("Попросить продлить дедлайн", url=f"tg://msg?text=Mi_mensaje&to=@{teacher['telegram_nickname']}")]) # Только для iOS.
+    keyboard.append([InlineKeyboardButton("К списку лабораторных", callback_data=f'course-{course_id}')])
+
+    return {"text": message, "reply_markup": InlineKeyboardMarkup(keyboard)}
 
 
+# Обработка нажатия любой кнопки.
 def button(update: Update, context: CallbackContext):
     query = update.callback_query
 
     query.answer()
 
-    if query.data == "navigation":
-        query.edit_message_text(text="Верхний блок навигации", reply_markup=navigation_markup())
+    # if query.data == "navigation":
+    #     query.edit_message_text(text="Верхний блок навигации", reply_markup=navigation_markup())
 
     if query.data == "courses":
         query.edit_message_text(text="Страница курсов", reply_markup=courses_markup(context))
 
+    # if query.data == "main":
+    #     query.edit_message_text(**main_markup(update, context))
 
     if query.data.startswith('course-'):
         id = query.data.split('-')[1]
-        r = requests.get(f'{API_URL}/course/{id}').json()
-        t = requests.get(r['teacher']).json()
-        message = f'''{r["name"]}
+        query.edit_message_text(**course_markup(id))
 
-Преподаватель: {t["name"]} (@{t["telegram_nickname"]}) [{t["table_link"]}]
-
-Группы: '''
-
-        groups = []
-        for g in r['course_groups']:
-            groups.append(g['name'])
-
-        message += ', '.join(groups)
-
-        message += f'\n\nПравила: {r["rules"]}'
-
-        labs = []
-        for lab in r['course_labs']:
-            labs.append(lab['number'])
-            message += f'{lab["number"]} с дедлайном {lab["deadline"]}, ссылки:\n'
-            for link in lab.get('links', []):
-                message += link['link'] + "\n"
-
-        query.edit_message_text(text=message)
-
+    if query.data.startswith('lab-'):
+        id = query.data.split('-')[1]
+        query.edit_message_text(**lab_markup(id))
 
 
 def main():
@@ -286,8 +320,8 @@ def main():
     dispatcher.add_handler(conv_handler) 
 
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("main", main_page)) # Для вызова через /
-    dispatcher.add_handler(CommandHandler("navigation", navigation))
+    # dispatcher.add_handler(CommandHandler("main", main_page)) # Для вызова через /
+    # dispatcher.add_handler(CommandHandler("navigation", navigation))
     dispatcher.add_handler(CommandHandler("courses", courses))
     dispatcher.add_handler(CallbackQueryHandler(button))
 
